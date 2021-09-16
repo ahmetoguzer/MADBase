6dfda89e560fbc641a65c130cd6d67e660ad78ad
ahmetoguzer <erahmetoguz@gmail.com>
2021-09-16 17:01:46 +0300

    renamed Application

--- app/src/main/java/com/ahmet/madbase/BaseApplicaiton.kt
+++ app/src/main/java/com/ahmet/madbase/BaseApplicaiton.kt
@@ -5,7 +5,7 @@ import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class BaseApplicaiton : Application() {
+class BaseApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
